c01c8283313d6f69e2ef3376ae82a28692c2fe11
Anton Benderskiy <loderan@ruservice.ru>
2023-10-09 18:13:19 +0300

    a few example code

--- myproj/myscript.py
+++ myproj/myscript.py
@@ -1,5 +1,16 @@
 import argparse
+import logging
 from pyramid.paster import bootstrap, setup_logging
+from pyramid_zodbconn import get_connection
+from pyramid.registry import Registry
+from pyramid.request import Request
+from ZODB.Connection import Connection
+
+# local imports
+from .settings import settings
+from .models import get_app_root
+
+log = logging.getLogger(__name__)
 
 
 def myfunction():
@@ -7,9 +18,25 @@ def myfunction():
     parser.add_argument('config_uri', help='The URI to the configuration file.')
     args = parser.parse_args()
 
+    # setup logging from config file settings
     setup_logging(args.config_uri)
+
+    # bootstrap Pyramid environment to get configuration
     with bootstrap(args.config_uri) as env:
-        settings = env['registry'].settings
+        registry: Registry = env['registry']
+        request: Request = env['request']
+
         print('Registry settings:')
-        for k, v in settings.items():
+        for k, v in registry.settings.items():
             print(f'{k} = {v}')
+
+        print(f'\nAccess to custom_myproj_parameter = {settings.custom_myproj_parameter}')
+
+        log.info('get database connection and App Root object')
+        conn: Connection = get_connection(request)
+        app_root = get_app_root(conn)
+        _ = app_root
+
+
+if __name__ == '__main__':
+    myfunction()
