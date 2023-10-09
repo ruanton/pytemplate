1873d56259252ebd6cb3e85cababecde01942503
Anton Benderskiy <loderan@ruservice.ru>
2023-10-09 16:35:28 +0300

    close ZODB on app exit

--- myproj/__init__.py
+++ myproj/__init__.py
@@ -1,9 +1,14 @@
+import atexit
+import logging
 import pyramid.config
 import pyramid_zodbconn
 import pyramid_tm
+import ZODB
 
 # local imports
 from . import models
+
+log = logging.getLogger(__name__)
 
 
 def root_factory(request):
@@ -11,6 +16,10 @@ def root_factory(request):
     """
     conn = pyramid_zodbconn.get_connection(request)
     return models.get_app_root(conn)
+
+
+# contains all ZODB database objects created by pyramid_zodbconn
+_zodbconn_databases: dict[str, ZODB.DB] | None = None
 
 
 def main(global_config, **settings):
@@ -32,4 +41,23 @@ def main(global_config, **settings):
         config.set_root_factory(root_factory)
         config.scan()
 
+    # save a link to ZODB database objects for closing on application exit
+    global _zodbconn_databases
+    _zodbconn_databases_current = getattr(config.registry, '_zodb_databases')
+    if _zodbconn_databases is not None and id(_zodbconn_databases) != id(_zodbconn_databases_current):
+        raise RuntimeError('_zodbconn_databases is already initialized with a dict with a different identity')
+    _zodbconn_databases = _zodbconn_databases_current
+
     return config.make_wsgi_app()
+
+
+# see: https://relstorage.readthedocs.io/en/latest/things-to-know.html#the-importance-of-closing-the-database
+# (unfortunately not called under Waitress even on reload)
+@atexit.register
+def zodb_close():
+    if _zodbconn_databases:
+        log.info('closing all ZODB database objects created by pyramid_zodbconn tween')
+        for db in _zodbconn_databases.values():
+            db.close()
+    else:
+        log.warning(f'zodb_close: no zodbconn database objects')
